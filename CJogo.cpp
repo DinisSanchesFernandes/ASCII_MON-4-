78de5a80519d5ac9898ed4c166aac4b3b349b08f
GayBotas <38889811+GayBotas@users.noreply.github.com>
2018-05-06 22:42:47 +0100

    Alterei um monte de cenas para o programa ficar funcional
    Linhas sublinhadas

--- CJogo.cpp
+++ CJogo.cpp
@@ -7,54 +7,89 @@
 #include "Pokemons.h"
 using namespace std;
 //EEIC
-
+void CJogo::introduzir_pokemon(){
+	string nome,tipo,especial;
+	int vida,ataque,numatack,velocidade;
+	cout<<"\nIntroduza o nome do pokemon: ";
+	cin>>nome;
+	
+	cout<<"\nIntroduza o tipo do pokemon: ";
+	cin>>tipo;
+	
+	cout<<"\nIntroduza o vida do pokemon: ";
+	cin>>vida;
+	
+	cout<<"\nIntroduza o ataque do pokemon: ";
+	cin>>ataque;
+	
+	cout<<"\nIntroduza o numero de ataues do pokemon: ";
+	cin>>numatack;
+	
+	cout<<"\nIntroduza o velocidade do pokemon: ";
+	cin>>velocidade;
+	
+	cout<<"\nIntroduza o especial do pokemon: ";
+	cin>>especial;
+	
+	ofstream fich("Pokedemons.txt");
+	if(fich.is_open()){
+		fich<<nome<<endl;
+		fich<<tipo<<endl;
+		fich<<vida<<endl;
+		fich<<ataque<<endl;
+		fich<<numatack<<endl;
+		fich<<velocidade<<endl;
+		fich<<especial<<endl;
+	}
+	else{
+		cout<<"\nNão foi possivel abrir o ficheiro.";
+	}
+}
 void CJogo::carregar_dados(){
-	string nome,tipo,str_vida,str_ataque,str_numatack,str_velocidade,especial;
-	int num_vida,num_ataque,num_numatack,num_velocidade;
+	string nome,tipo,str_vida,str_ataque,str_numatack,str_velocidade,especial,nome_ataque;
+	int num_vida,num_ataque,num_numatack,num_velocidade,i,j;
+	i=0;
 	ifstream fich("Pokedex.txt");
 	if(fich.is_open()){
-		while(!fich.eof()){
-			Pokemons P;
-
+		while(fich.good()){
+		Pokemons P;
+		i++;
 			getline(fich,nome);
 			P.set_Name(nome);
-//			cout<<"\nNome: "<<nome;
-			cout<<P.get_Name();
-
 			
 			getline(fich,tipo);
 			P.set_Type(tipo);
-//			cout<<"\nTipo: "<<tipo;
-
 			
 			getline(fich,str_vida);
-			stringstream(str_vida) >> num_vida;
-//			cout<<"\nnum_vida: "<<num_vida;
-
+			istringstream ( str_vida ) >> num_vida;
+			P.set_Health(num_vida);
+			
 			getline(fich,str_ataque);
-			stringstream(str_ataque) >> num_ataque;
+			istringstream(str_ataque) >> num_ataque;
 			P.set_Attack(num_ataque);
-//			cout<<"\nnum_ataque: "<<num_ataque;
-
-
-			getline(fich,str_velocidade);
-			stringstream(str_velocidade) >> num_velocidade;
-			P.set_Speed(num_velocidade);
-//			cout<<"\nVelocidade: "<<num_velocidade;
-
-
+						 
 			getline(fich,str_numatack);
-			stringstream(str_numatack) >> num_numatack;
-			P.set_Attack(num_numatack);
-//			cout<<"\nNumero de ataques: "<<num_numatack;
+			istringstream(str_numatack) >> num_numatack;
+			P.set_Numatack(num_numatack);
+			
+			
+			for(j=0;j<num_numatack;j++){
+				getline(fich,nome_ataque);
+				P.set_nomeAtaques(nome_ataque);
+			}
 
-
+			
+			getline(fich,str_velocidade);
+			istringstream(str_velocidade) >> num_velocidade;
+			P.set_Speed(num_velocidade);
+			
 			getline(fich,especial);
 			P.set_Special(especial);
-//			cout<<"\nEspecial: "<<especial;
 			
 			pokemons.push_back(P);
+
 		}
+		pokemons.erase(pokemons.begin()+i);		
 	}
 	else{
 		cout<<"\nNão foi possivel abrir o ficheiro";
@@ -64,9 +99,15 @@ void CJogo::carregar_dados(){
 }
 void CJogo::mostrar_dados(){
 	int i;
-//	i=0;
-	Pokemons P=pokemons[0];
-	cout<<P.get_Name();
-	
-
+	cout<<"\nVetor: "<<pokemons.size();
+	for(i=0;i<pokemons.size();i++){
+		Pokemons P=pokemons[i];
+		cout<<"\n------"<<P.get_Name()<<"------";
+		cout<<"\nTipo: "<<P.get_Type();
+		cout<<"\nVida: "<<P.get_Health();
+		cout<<"\nAtaque: "<<P.get_Attack();
+		cout<<"\nNumero de ataques: "<<P.get_Numatack();
+		cout<<"\nVelocidade: "<<P.get_Speed();
+		cout<<"\nEspecial: "<<P.get_Special();
+	}
 }
